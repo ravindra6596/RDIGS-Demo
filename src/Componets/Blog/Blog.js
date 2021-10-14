287b751abb538d72fe3423ce1ace426e798bd087
8855078311 <sonalikoli6@gmail.com>
2021-10-14 18:54:59 +0530

    company profile

--- src/Componets/Blog/Blog.js
+++ src/Componets/Blog/Blog.js
@@ -2,38 +2,8 @@ import React from "react";
 import "./Blog.css";
 import Bannercommon from "../Bannercommon/Bannercommon";
 import cardimg2 from "../../img/blog/cardimg2.jpg";
-import card4 from "../../img/blog/card4.jpg";
 import blog4 from "../../img/blog/blog4.jpg";
 const Blog = () => {
-  const blog = [
-    {
-      cardimg: card4,
-      day: "03",
-      month: "OCT",
-      year: "2021",
-      blogtitle: "Blog Title",
-      bloginfo:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    },
-    {
-      cardimg: card4,
-      day: "03",
-      month: "OCT",
-      year: "2021",
-      blogtitle: "Blog Title",
-      bloginfo:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    },
-    {
-      cardimg: card4,
-      day: "03",
-      month: "OCT",
-      year: "2021",
-      blogtitle: "Blog Title",
-      bloginfo:
-        "Some quick example text to build on the card title and make up the bulk of the card's content.",
-    },
-  ];
   const carddata=[
     {
       cardpic:cardimg2,
@@ -63,64 +33,13 @@ const Blog = () => {
   return (
     <>
       <div className="nav-contaniner"/>
-      <Bannercommon title="blog"/>
-      <section>
-          <div className="container">
-            <div className="row">
-              {blog.map((user, index) => {
-                return (
-                  <div className="col-xs-6 col-sm-6 col-md-4 col-lg-4">
-                    <div
-                      className="card-general"
-                      data-aos="flip-left"
-                      data-aos-easing="ease-out-cubic"
-                      data-aos-duration="300"
-                    >
-                      <img
-                        src={user.cardimg}
-                        className="cardblogimg img-fluid"
-                        alt="blog-img"
-                      />
-                      <div
-                        className="round-div-card"
-                        data-aos="fade-up"
-                        data-aos-duration="500"
-                      >
-                        <h2 className="card-date">{user.day}</h2>
-                        <h3 className="card-month">{user.month}</h3>
-                        <h5 className="card-year">{user.year}</h5>
-                      </div>
-                      <div className="card-body">
-                        <h3
-                          className="card-blog-title"
-                          data-aos="fade-down"
-                          data-aos-easing="linear"
-                          data-aos-duration="1200"
-                        >
-                          {user.blogtitle}
-                        </h3>
-                        <p
-                          className="card-text text-center"
-                          data-aos="fade-down"
-                          data-aos-easing="linear"
-                          data-aos-duration="1500"
-                        >
-                          {user.bloginfo}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-      </section>
+      <Bannercommon title="BLOG"/>
       <section className="blog4-section">
         <div className="container">
               <div className="row blog4-img-row">
                 <div className="col-sm-12 col-md-6 col-lg-6">
                   <div className="blog-4img-div">
-                    <img src={blog4} alt="blog4"></img>
+                    <img src={blog4} alt="blog4" className="img-fluid"></img>
                   </div>
                 </div>
                 <div className="col-sm-12 col-md-6 col-lg-6 blog4-content">
